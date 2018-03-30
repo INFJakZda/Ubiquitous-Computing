1ff229af922535d2d7ba3cd465e47ea717c8ef0a
INFJakZda <zdanowski78@gmail.com>
2018-03-30 23:44:21 +0200

    Add layout sample - configure constraint (1.4)

---
+++ LayoutSample/app/src/main/java/com/zdano/layoutsample/LayoutSample.kt
@@ -0,0 +1,12 @@
+package com.zdano.layoutsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutSample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_sample)
+    }
+}
