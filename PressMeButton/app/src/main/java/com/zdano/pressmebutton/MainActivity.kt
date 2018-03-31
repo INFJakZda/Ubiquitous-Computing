7577335413d37f108b34cd157f72ffda1d953061
INFJakZda <zdanowski78@gmail.com>
2018-03-31 15:55:18 +0200

    Change button manage - add wait thread to button (2.4)

--- PressMeButton/app/src/main/java/com/zdano/pressmebutton/MainActivity.kt
+++ PressMeButton/app/src/main/java/com/zdano/pressmebutton/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.zdano.pressmebutton
 
+import android.app.Activity
+import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,15 +14,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        pressMeButton.setOnClickListener {
-            statusText.text = "Nacisnieto"
-        }
-        pressMeButton.setOnLongClickListener {
-            statusText.text = "Dlugie wcisniecie"
-            true
-        }
     }
 
+    fun onClic(v: View) {
+        var i = 0
+        while (i <= 20) {
+            try {
+                Thread.sleep(10)
+                i++
+            } catch (e: Exception) {
 
+            }
+        }
+        statusText.text = "Skończyłem"
+    }
 }
