633818e0fe2e87c79a6acafe3be26f7e533735a9
INFJakZda <zdanowski78@gmail.com>
2018-04-04 02:35:28 +0200

    Add missing layouts

---
+++ BroadcastSample/app/src/main/java/com/zdano/broadcastsample/SendBroadcastActivity.kt
@@ -0,0 +1,12 @@
+package com.zdano.broadcastsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SendBroadcastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_broadcast)
+    }
+}
